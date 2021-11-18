f7dc056db8ed9bcc4b360ecd428c3928f342c1b6
mbaissset <marc.baisset@gmail.com>
2021-11-18 16:08:05 +0100

    Initial implementation of the Paque.java

--- src/Paque.java
+++ src/Paque.java
@@ -4,7 +4,10 @@ public class Paque {
     private final static Scanner CLAVIER = new Scanner(System.in);
 
     public static void main (String[] args) {
-        demanderAnnee();
+
+        int year = demanderAnnee();
+        int nbDays = datePaques(year);
+        afficheDate(year, nbDays);
 
     }
 
@@ -19,5 +22,40 @@ public class Paque {
         return annee;
     }
 
+    static void afficheDate (int pAnnee, int pNbJours) {
+        String mois;
+
+        int jour = 0;
+
+
+        if (pNbJours <= 31) {
+            mois = "mars";
+            jour = pNbJours;
+        } else {
+            mois = "avril";
+            jour = pNbJours - 31;
+        }
+
+        System.out.print("Date de Paques en " + pAnnee + ": " + jour + " " + mois);
+
+    }
+
+    static int datePaques (int year){
+
+        int siecle = year / 100;
+        int p = (13 + 8 * siecle) / 25;
+        int q = siecle / 4;
+        int m = (15 - p + siecle - q) % 30;
+        int n = (4 + siecle - q) % 7;
+        int d = (m + 19 * (year % 19)) % 30;
+        int e = (2 * (year % 4) + 4 * (year % 7) + 6 * d + n) % 7;
+        int day = e + d +22;
+
+        if ((e ==6) && ((d == 29) || ((d == 28) && ((11 * (m +1) % 30) < 19)))){
+            day -= 7;
+        }
+
+        return day;
+    }
 
 }
