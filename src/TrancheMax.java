6f730f25bf9de9cc45c2b6743a6e92b116feb6dd
mbaisset <marc.baisset@gmail.com>
2021-11-20 23:19:31 +0100

    Initial implementation of TrancheMax.java

--- src/TrancheMax.java
+++ src/TrancheMax.java
@@ -40,12 +40,73 @@ class TrancheMax {
      *******************************************/
 
     private static boolean checkFormat(String matrice) {
-        return false;
+
+        if (matrice.length() == 0) {
+            System.out.println("Matrice vide !");
+            return false;
+        }
+
+        for (int i = 0; i < matrice.length(); i++){
+            switch (matrice.charAt(i)) {
+                case '0': case ' ': case '1':
+                    break;
+
+                default:
+                    System.out.println("Matrice invalide : seuls '1', '0' et ' ' sont admis !");
+                    return false;
+            }
+        }
+        return checkLineLength(matrice);
     }
 
     private static ArrayList<Integer> findConsecutiveList(String matrice) {
         ArrayList<Integer> consecutiveList = new ArrayList<>();
+
+        int maxUn = 0;
+        String[] matriceSplitted = matrice.trim().split(" {1,}");
+        for (int i = 0; i < matriceSplitted.length; i++){
+            int nbUnConsecutif = 0;
+            int nbMaxUnConsecutifLigne = 0;
+            for (int j = 1; j < matriceSplitted[i].length(); j++){
+                if (matriceSplitted[i].charAt(j) == '1'  && (matriceSplitted[i].charAt(j-1)) == '1'){
+                    nbUnConsecutif++;
+                    if (nbUnConsecutif > nbMaxUnConsecutifLigne) {
+                        nbMaxUnConsecutifLigne = nbUnConsecutif;
+                    }
+                } else {
+                    nbUnConsecutif = 0;
+                }
+
+            }
+            if (nbMaxUnConsecutifLigne > maxUn) {
+                maxUn = nbMaxUnConsecutifLigne;
+                consecutiveList.clear();
+                consecutiveList.add(i);
+            } else if (nbMaxUnConsecutifLigne == maxUn){
+                consecutiveList.add(i);
+            }
+        }
+
+
         return  consecutiveList;
+    }
+
+    private static boolean checkLineLength (String matriceLine){
+        String[] matriceLineSplitted = matriceLine.trim().split(" {1,}");
+        int basicLength = 0;
+
+        if (matriceLineSplitted.length > 0) {
+            basicLength = matriceLineSplitted[0].length();
+        }
+
+        for (int i = 1; i < matriceLineSplitted.length; i++){
+            if (matriceLineSplitted[i].length() != basicLength) {
+                System.out.println("Matrice invalide, lignes de longueurs differentes !");
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /*System.out.println("Matrice invalide, lignes de longueurs differentes !");
