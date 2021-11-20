788073071591af4859808bcf1b602c8a323e46a8
mbaisset <marc.baisset@gmail.com>
2021-11-21 00:18:29 +0100

    Fixing implementation issues of TrancheMax.java

--- src/TrancheMax.java
+++ src/TrancheMax.java
@@ -33,22 +33,22 @@ class TrancheMax {
     }
 
 
-
-
     /*******************************************
      * Completez le programme a partir d'ici.
      *******************************************/
 
-    private static boolean checkFormat(String matrice) {
+    static boolean checkFormat(String matrice) {
 
         if (matrice.length() == 0) {
             System.out.println("Matrice vide !");
             return false;
         }
 
-        for (int i = 0; i < matrice.length(); i++){
+        for (int i = 0; i < matrice.length(); i++) {
             switch (matrice.charAt(i)) {
-                case '0': case ' ': case '1':
+                case '0':
+                case ' ':
+                case '1':
                     break;
 
                 default:
@@ -59,39 +59,47 @@ class TrancheMax {
         return checkLineLength(matrice);
     }
 
-    private static ArrayList<Integer> findConsecutiveList(String matrice) {
+    static ArrayList<Integer> findConsecutiveList(String matrice) {
         ArrayList<Integer> consecutiveList = new ArrayList<>();
 
         int maxUn = 0;
         String[] matriceSplitted = matrice.trim().split(" {1,}");
-        for (int i = 0; i < matriceSplitted.length; i++){
+        for (int i = 0; i < matriceSplitted.length; i++) {
             int nbUnConsecutif = 0;
             int nbMaxUnConsecutifLigne = 0;
-            for (int j = 1; j < matriceSplitted[i].length(); j++){
-                if (matriceSplitted[i].charAt(j) == '1'  && (matriceSplitted[i].charAt(j-1)) == '1'){
-                    nbUnConsecutif++;
-                    if (nbUnConsecutif > nbMaxUnConsecutifLigne) {
-                        nbMaxUnConsecutifLigne = nbUnConsecutif;
-                    }
-                } else {
-                    nbUnConsecutif = 0;
-                }
 
+            if (matriceSplitted[i].length() == 1) {
+                if (matriceSplitted[i].charAt(0) == '1') {
+                    nbMaxUnConsecutifLigne = 1;
+                }
+            } else {
+
+
+                for (int j = 1; j < matriceSplitted[i].length(); j++) {
+                    if (matriceSplitted[i].charAt(j) == '1' && (matriceSplitted[i].charAt(j - 1)) == '1') {
+                        nbUnConsecutif++;
+                        if (nbUnConsecutif > nbMaxUnConsecutifLigne) {
+                            nbMaxUnConsecutifLigne = nbUnConsecutif;
+                        }
+                    } else {
+                        nbUnConsecutif = 0;
+                    }
+                }
             }
             if (nbMaxUnConsecutifLigne > maxUn) {
                 maxUn = nbMaxUnConsecutifLigne;
                 consecutiveList.clear();
                 consecutiveList.add(i);
-            } else if (nbMaxUnConsecutifLigne == maxUn){
+            } else if (nbMaxUnConsecutifLigne == maxUn && nbMaxUnConsecutifLigne != 0) {
                 consecutiveList.add(i);
             }
         }
 
 
-        return  consecutiveList;
+        return consecutiveList;
     }
 
-    private static boolean checkLineLength (String matriceLine){
+    static boolean checkLineLength(String matriceLine) {
         String[] matriceLineSplitted = matriceLine.trim().split(" {1,}");
         int basicLength = 0;
 
@@ -99,7 +107,7 @@ class TrancheMax {
             basicLength = matriceLineSplitted[0].length();
         }
 
-        for (int i = 1; i < matriceLineSplitted.length; i++){
+        for (int i = 1; i < matriceLineSplitted.length; i++) {
             if (matriceLineSplitted[i].length() != basicLength) {
                 System.out.println("Matrice invalide, lignes de longueurs differentes !");
                 return false;
